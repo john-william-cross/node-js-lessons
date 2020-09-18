bf079b886cd6a6b17ac14407c69546857c15b87b
John Cross <john.william.cross@gmail.com>
2020-09-17 21:40:58 -0700

    second initial commit

--- string-methods.js
+++ string-methods.js
@@ -3,10 +3,10 @@ const name = `  John Cross           `;
 const trimmedName = name.trim();
 console.log(`The trimmed name is ${trimmedName}.`);
 
-const startTrimmedName = name.trimStart();
+const startTrimmedName = name.trimStart(); // can also use trimLeft()
 console.log(`The start/left trimmed name is ${startTrimmedName}.`);
 
-const endTrimmedName = name.trimEnd();
+const endTrimmedName = name.trimEnd(); // can also use trimRight()
 console.log(`The end/righttrimmed  name is ${endTrimmedName}.`);
 
 const nameLength = trimmedName.length;
