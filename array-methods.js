4111716a0dd2bb6dace0973f7fcfe93155e41e69
John Cross <john.william.cross@gmail.com>
2020-09-19 09:20:26 -0700

    finish the array-methods video notes: includes add how to replace things, creating lists, new lists, seeing if stuff is included in a list.

--- array-methods.js
+++ array-methods.js
@@ -32,14 +32,44 @@ console.log(
    `${favoriteHoliday} is found at the ${indexOfIndependenceDay} of the holidays array.`
 );
 
-const specificHoliday = `Memorial Day (Last Monday in May)`;
+const specificHoliday = `John's birthday (May 2)`;
 
 //how to find if something is in a list or not using index
-if (holidays.indexOf(specificHoliday) === -1) {
-   console.log(`No, ${specificHoliday} is not a holiday.`);
-} else {
+// if (holidays.indexOf(specificHoliday) === -1) {
+//    console.log(`No, ${specificHoliday} is not a holiday.`);
+// } else {
+//    console.log(
+//       `Yes, ${specificHoliday} is a holiday. It was found at the
+//       ${holidays.indexOf(specificHoliday)} index in the list of holidays}`
+//    );
+// }
+//works the same with lastIndexOf() method; it just searches backwords from right to left
+
+//How to do this using .includes:
+if (holidays.includes(specificHoliday)) {
    console.log(
-      `Yes, ${specificHoliday} is a holiday. It was found at the
-      ${holidays.indexOf(specificHoliday)} index in the list of holidays}`
+      `Yes, ${specificHoliday} is a holiday. It was found at the ${holidays.indexOf(
+         specificHoliday
+      )} index in the list of holidays}.`
    );
+} else {
+   console.log(`No, ${specificHoliday} is not a holiday.`);
 }
+
+const replaceableHoliday = `Washington's Birthday (Third Monday in February)`;
+//if you ever want to change something in an array, you need to know its index
+const indexOfReplaceableHoliday = holidays.indexOf(replaceableHoliday);
+console.log(
+   `We are going to replace ${replaceableHoliday} at the ${indexOfReplaceableHoliday} index of the holidays list`
+);
+
+const newHolidays = [
+   ...holidays,
+]; /*use spread element (...) to make a copy of holidays array
+without spread element, we are just putting an array inside an array. Spread element unpacks the holidays
+array and adds each item into this new array*/
+newHolidays[indexOfReplaceableHoliday] = specificHoliday;
+console.log(`Here's a list of our old holidays:\n `, holidays);
+console.log(`Here's a list of our new holidays:\n `, newHolidays);
+
+//another way to copy an array is Array.from: on line 66 it would look like this: const newHolidays = Array.from(holidays)
