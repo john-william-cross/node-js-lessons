d64622ccf817df7bb0f1c6b946876af36a5a86aa
John Cross <john.william.cross@gmail.com>
2020-09-21 16:00:37 -0700

    notes up to 19 minute mark

---
+++ reversing-and-normalizing-strings.js
@@ -0,0 +1,42 @@
+const email = "mike@gmail.com";
+
+//**** Split converts a string to an array ********/
+
+// const listOfEmailChars = email.split(``);
+// console.log(listOfEmailChars);
+// //outputs ['m', 'i', 'k', 'e', '@', 'g', 'm', 'a', 'i', 'l', '.', 'c', 'o', 'm']
+// console.log(listOfEmailChars[0]);
+// //outputs m
+
+// listOfEmailChars = email.split(); //same as [email]
+// console.log(listOfEmailChars);
+// //outputs [`mike@gmail.com`]
+
+// const listOfEmailChars = email.split(`@`);
+// console.log(listOfEmailChars);
+// //outputs [ 'mike', 'gmail.com' ]
+
+// const todaysDate = `2020-09-11`;
+// const dateParts = todaysDate.split(`-`);
+// // console.log(dateParts);
+// //outputs [ `2020`, `09`, `11` ]
+// const year = dateParts[0];
+// const month = dateParts[1];
+// const day = dateParts[2];
+// console.log(year, month, day);
+// //outputs 2020 09 11
+
+const greeting = `hello`;
+const greetingChars = greeting.split(``);
+console.log(greetingChars);
+//outputs [ 'h', 'e', 'l', 'l', 'o' ]
+const copyOfGreetingChars = [...greetingChars];
+const reverseGreetingChars = copyOfGreetingChars.reverse();
+//reverse destroys original data so make a copy
+console.log(`Reversed chars: `, reverseGreetingChars);
+//outputs Reversed chars:  [ 'o', 'l', 'l', 'e', 'h' ]
+console.log(`original chars: `, greetingChars);
+//outputs original chars:  [ 'h', 'e', 'l', 'l', 'o' ]
+const newGreeting = reverseGreetingChars.join(``);
+console.log(`Here's our new greeting:\n`, newGreeting);
+//outputs Here's our new greeting: olleh
