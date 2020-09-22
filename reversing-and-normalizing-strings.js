cbac18a0be1ac3e6409f4eff24de7b7ca72f8cd2
John Cross <john.william.cross@gmail.com>
2020-09-22 11:33:31 -0700

    uncommented some things

--- reversing-and-normalizing-strings.js
+++ reversing-and-normalizing-strings.js
@@ -7,14 +7,15 @@ const email = "mike@gmail.com";
 // //outputs ['m', 'i', 'k', 'e', '@', 'g', 'm', 'a', 'i', 'l', '.', 'c', 'o', 'm']
 // console.log(listOfEmailChars[0]);
 // //outputs m
+// console.log(typeof email);
 
 // listOfEmailChars = email.split(); //same as [email]
 // console.log(listOfEmailChars);
 // //outputs [`mike@gmail.com`]
-
-// const listOfEmailChars = email.split(`@`);
-// console.log(listOfEmailChars);
-// //outputs [ 'mike', 'gmail.com' ]
+console.log(email);
+const listOfEmailChars = email.split(`@`);
+console.log(listOfEmailChars);
+//outputs [ 'mike', 'gmail.com' ]
 
 // const todaysDate = `2020-09-11`;
 // const dateParts = todaysDate.split(`-`);
@@ -24,35 +25,35 @@ const email = "mike@gmail.com";
 // const month = dateParts[1];
 // const day = dateParts[2];
 // console.log(year, month, day);
-// //outputs 2020 09 11
+//outputs 2020 09 11
 
-const greeting = `hello`;
-const greetingChars = greeting.split(``);
-//console.log(greetingChars);
-//outputs [ 'h', 'e', 'l', 'l', 'o' ]
-const copyOfGreetingChars = [...greetingChars];
-const reverseGreetingChars = copyOfGreetingChars.reverse();
-//reverse destroys original data so make a copy
-//console.log(`Reversed chars: `, reverseGreetingChars);
-//outputs Reversed chars:  [ 'o', 'l', 'l', 'e', 'h' ]
-//console.log(`original chars: `, greetingChars);
-//outputs original chars:  [ 'h', 'e', 'l', 'l', 'o' ]
-const newGreeting = reverseGreetingChars.join(``);
-//console.log(`Here's our new greeting:\n`, newGreeting);
-//outputs Here's our new greeting: olleh
+// const greeting = `hello`;
+// const greetingChars = greeting.split(``);
+// //console.log(greetingChars);
+// //outputs [ 'h', 'e', 'l', 'l', 'o' ]
+// const copyOfGreetingChars = [...greetingChars];
+// const reverseGreetingChars = copyOfGreetingChars.reverse();
+// //reverse destroys original data so make a copy
+// //console.log(`Reversed chars: `, reverseGreetingChars);
+// //outputs Reversed chars:  [ 'o', 'l', 'l', 'e', 'h' ]
+// //console.log(`original chars: `, greetingChars);
+// //outputs original chars:  [ 'h', 'e', 'l', 'l', 'o' ]
+// const newGreeting = reverseGreetingChars.join(``);
+// //console.log(`Here's our new greeting:\n`, newGreeting);
+// //outputs Here's our new greeting: olleh
 
-const query = "           GOOGLe FONTS        ";
-const dbEntries = [`    Google Fonts    `, `Google Maps`];
-// const dbEntry = dbEntries[0];
-// const lowerCasedDbEntry = dbEntry.toLowerCase(); // `google fonts`
-// console.log(lowerCasedDbEntry);
-// const trimmedDbEntry = lowerCasedDbEntry.trim();
-// console.log(trimmedDbEntry);
-// const lowerCasedQuery;
-// const trimmedQuery
-const isInDb = query.toLowerCase().trim() === dbEntries[0].toLowerCase().trim();
-if (isInDb) {
-   console.log(`We found a result!`);
-} else {
-   console.log(`Sorry, couldn't find anything for ${query}`);
-}
+// const query = "           GOOGLe FONTS        ";
+// const dbEntries = [`    Google Fonts    `, `Google Maps`];
+// // const dbEntry = dbEntries[0];
+// // const lowerCasedDbEntry = dbEntry.toLowerCase(); // `google fonts`
+// // console.log(lowerCasedDbEntry);
+// // const trimmedDbEntry = lowerCasedDbEntry.trim();
+// // console.log(trimmedDbEntry);
+// // const lowerCasedQuery;
+// // const trimmedQuery
+// const isInDb = query.toLowerCase().trim() === dbEntries[0].toLowerCase().trim();
+// if (isInDb) {
+//    console.log(`We found a result!`);
+// } else {
+//    console.log(`Sorry, couldn't find anything for ${query}`);
+// }
