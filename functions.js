86d85b53e401cb2437aab603acb847e9e5c8757a
John Cross <john.william.cross@gmail.com>
2020-09-23 07:36:32 -0700

    commit at 28:37, right before he talks about `throw`

--- functions.js
+++ functions.js
@@ -10,10 +10,46 @@
 // Reverse it
 // Output that reversed string
 
+// function toReverse(str) {
+//    return str; // return str[0] would return H from line 17 below
+// }
+
+// const reversedString = toReverse(`Hello`);
+
+// console.log(reversedString);
+
+// function toReverse(str) {
+//    if (typeof str === `string`) {
+//       const chars = str.split(""); //first we have to split the string, making it into an array of substrings
+//       //    console.log(chars);
+//       const reversedChars = chars.reverse(); //then run the reverse() function on the string
+//       //    console.log(reversedChars);
+//       const reversedStr = reversedChars.join(""); //join(``) puts each substring back into one string
+//       //    console.log(reversedStr);
+//       return reversedStr;
+//    } else {
+//       return `Error: please input a string`;
+//    }
+// }
+
+// const reversedString = toReverse(`john`);
+// console.log(reversedString);
+
+/* ANOTHER WAY TO DO WHAT'S ABOVE */
+
 function toReverse(str) {
-   return str; // return str[0] would return H from line 17 below
+   if (str === ``) {
+      return `Error: String cannot be empty`;
+   }
+   try {
+      const chars = str.split("");
+      const reversedChars = chars.reverse();
+      const reversedStr = reversedChars.join(``);
+      return reversedStr;
+   } catch {
+      return `Error: please input a string`; //what if input is not defined, i.e. toReverse(apple)
+   }
 }
 
-const reversedString = toReverse(`Hello`);
-
+const reversedString = toReverse();
 console.log(reversedString);
