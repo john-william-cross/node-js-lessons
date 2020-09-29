7216fdbba7f1eabaadeba4a7b9603f9c07f02e36
John Cross <john.william.cross@gmail.com>
2020-09-29 07:33:58 -0700

    begin notes on Math and numbers

---
+++ numbers.js
@@ -0,0 +1,7 @@
+const priceOfSquirtgun = 3.5;
+const floorOfSquirtgun = Math.floor(priceOfSquirtgun);
+console.log(`the floor of this item is: ${floorOfSquirtgun}`);
+const ceilOfSquirtgun = Math.ceil(priceOfSquirtgun);
+console.log(`the ceiling of this item is: ${ceilOfSquirtgun}`);
+const roundedPrice = Math.round(priceOfSquirtgun);
+console.log(`the rounded price of this itme is: ${roundedPrice}`);
