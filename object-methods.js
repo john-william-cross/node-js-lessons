92be2bcc2304df7628dd21217f06e24666ad7e03
John Cross <john.william.cross@gmail.com>
2020-09-30 10:13:49 -0700

    finish object methods video, begin CRUD with objects video

--- object-methods.js
+++ object-methods.js
@@ -9,3 +9,45 @@ const memoryCard = {
    totalSuccessfulAttempts: 1,
    userId: `50aab22f-8e8e-49c2-8d16-e1039febf29e`,
 };
+
+// method for getting all the keys
+
+// Object.keys()   //you pass this method/function the object to get all its keys
+const keys = Object.keys(memoryCard); //create variable called keys, store all memoryCard keys there
+// console.log(keys); //log keys
+
+// Object.values()  //you pass this method/function the object to get all its values
+const values = Object.values(memoryCard); //create variable called values, store all memoryCard values there
+// console.log(values); //log values
+
+const hasAnswerProp = memoryCard.hasOwnProperty(`answer`); //check if the object, in this case memoryCard, has an answer property/key
+// console.log(hasAnswerProp);
+
+// function checkObj(obj, checkProp) {
+//     const
+//     if (obj)
+// }
+
+const items = {
+   gift: `pony`,
+   pet: `kitten`,
+   bed: `sleigh`,
+};
+
+function checkObj(obj, checkProp) {
+   const keys = Object.keys(obj);
+   console.log(keys);
+   const values = Object.values(obj);
+   console.log(values);
+   const hasCheckPropProperty = obj.hasOwnProperty(checkProp);
+
+   console.log(`This property is present: `, hasCheckPropProperty);
+
+   if (hasCheckPropProperty) {
+      console.log(checkProp);
+   } else {
+      console.log(`Not Found`);
+   }
+}
+
+checkObj(items, `gift`);
