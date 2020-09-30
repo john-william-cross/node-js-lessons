bb3c031d8a137277fac2ba87be4148ef0ca984f6
John Cross <john.william.cross@gmail.com>
2020-09-30 07:12:15 -0700

    begin object methods video

---
+++ object-methods.js
@@ -0,0 +1,11 @@
+const memoryCard = {
+   answer: `The technical term for hashtag is octothorpe`,
+   createdAt: 1601474982458,
+   id: `6002a816-a85d-4332-985d-6da576a1ce5c`,
+   imagery: `Jim Thorpe has octopus arms and is cooking hashbrowns`,
+   lastAttemptAt: 1601474996936,
+   level: 1,
+   nextAttemptAt: 1601475008675,
+   totalSuccessfulAttempts: 1,
+   userId: `50aab22f-8e8e-49c2-8d16-e1039febf29e`,
+};
--- objects.js
+++ objects.js
@@ -1,8 +1,31 @@
-const userProps = [`mike@gmail.com`, `mikepassword`, 20200918, `005061`];
-
 const user = {
-   email: `mike@gmail.com`,
-   password: `mikepassword`,
-   createdAt: 20200918,
-   id: `005061`,
+   email: getEmail(),
+   password: getPassword(),
+   createdAt: getCreatedAt(),
+   id: getId(),
 };
+
+console.log(user);
+
+/// functions start here ///
+
+function getEmail() {
+   // get the user's email from the input with jQuery
+   return `mike@gmail.com`;
+}
+
+function getPassword() {
+   // get the user's password from the input with jQuery
+
+   return `mikepassword`;
+}
+
+function getCreatedAt() {
+   // generate createdAt number, e.g. 20200921
+   return 20200921;
+}
+
+function getId() {
+   // generate id string, e.g. `005061`
+   return `005061`;
+}
