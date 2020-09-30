b004ecd01fe0982976d886a38a30eb9a278ce643
John Cross <john.william.cross@gmail.com>
2020-09-30 06:09:42 -0700

    begin objects video

---
+++ objects.js
@@ -0,0 +1,8 @@
+const userProps = [`mike@gmail.com`, `mikepassword`, 20200918, `005061`];
+
+const user = {
+   email: `mike@gmail.com`,
+   password: `mikepassword`,
+   createdAt: 20200918,
+   id: `005061`,
+};
