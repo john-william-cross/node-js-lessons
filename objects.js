7a3207abcf2092ddbe13597c6ad51fd77295f93b
John Cross <john.william.cross@gmail.com>
2020-09-30 10:54:08 -0700

    making copies of and updating objects

--- objects.js
+++ objects.js
@@ -5,7 +5,29 @@ const user = {
    id: getId(),
 };
 
-console.log(user);
+// adding properties to an object does mutate the object, so we have to make a copy first
+
+// you can use the ... (spread operator) on an object to make a copy
+
+console.log(`Original user: `, user);
+
+const copyOfUser = {
+   //this is a shallow copy
+   ...user,
+};
+
+copyOfUser.isActive = true; // this adds an `isActive` key/property with a value of true
+copyOfUser.tokenExpiredAt = user.createdAt + 86400000; //gets us the value at the property/key `createdAt`
+//and adds 24 hours (in milliseconds) to that time, hence making it expire in 24 hours
+copyOfUser.isCoolPerson = true;
+
+copyOfUser.isCoolPerson = false; //change this value to false; only logs the last value of the property
+
+console.log(`Copy of original user: `, copyOfUser);
+
+//to delete a property from an object
+delete copyOfUser.isCoolPerson;
+console.log(`Copy of original user: `, copyOfUser);
 
 /// functions start here ///
 
@@ -21,8 +43,8 @@ function getPassword() {
 }
 
 function getCreatedAt() {
-   // generate createdAt number, e.g. 20200921
-   return 20200921;
+   // generate createdAt number, e.g. 160020200921
+   return Date.now();
 }
 
 function getId() {
