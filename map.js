d17ffb9caaaa437dc04e55a3d1069668b2749969
John Cross <john.william.cross@gmail.com>
2020-10-02 13:49:49 -0700

    solution to fcc map challenge

--- map.js
+++ map.js
@@ -1,37 +1,33 @@
 const users = [
    {
-      name: `john`,
+      name: `John`,
       isActive: true,
    },
    {
-      name: `mike`,
+      name: `Mike`,
       isActive: true,
    },
    {
-      name: `michelle`,
+      name: `Michelle`,
       isActive: false,
    },
 ];
 
-// map allows us to transform each thing within a list of things all together
-// if we wanted to add another property to all of the users..
+// map transforms data. you have to assign its result to
+// a constant
 
-//map over this data and do what's in the function
-const formattedUsers = users.map((user) => {
-   //map has no side effects
-   //    return user.name; //turns list of objects into list of strings
-   const newUser = { ...user };
-   newUser.createAt = Date.now(); //adding something to each user object... if you do something in
-   //the codeblock, it DOES update the original array. So in this case, we make a shallow copy on
-   //line 23 since the data is only one level deep
-   delete newUser.isActive; //subtracting isActive from all objects...
-   newUser.name = user.name.toLowerCase();
-   return newUser; //this is a new array
-
-   //if you just wanted to make a list of strings it would not mutate the original
+// const newUsers = users.map(() => {}) skeleton of map function
+// map returns a new array doing whatever changes you ask it to
+const newUsers = users.map((user) => {
+   //for every user, in this list of users, return a user
+   //    return user.name; //returns just a list of objects as a list of strings
+   const newUser = { ...user }; // if you do something inside the code block it will update the old array
+   // we have to make a copy!!!
+   newUser.createdAt = Date.now(); //adds a new property to user called createdAt
+   delete newUser.isActive; // removes a property
+   newUser.name = user.name.toLowerCase(); // updates a property
+   return newUser;
 });
 
-console.log(formattedUsers); //does not change users at all; map returns a new array doing
-//whatever changes you ask it to do
-
-console.log(users);
+console.log(`new users: `, newUsers);
+console.log(`users: `, users);
