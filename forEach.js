0c01ebe87f56305a0fa96c3e244f8918a3e33638
John Cross <john.william.cross@gmail.com>
2020-10-02 08:59:49 -0700

    practice some stuff

--- forEach.js
+++ forEach.js
@@ -1,14 +1,75 @@
-function getUserId(num) {
-   return num;
-}
+// function getUserId(num) {
+//    return num;
+// }
 
-function getUser(callback) {
-   const userId = callback(Date.now());
-   return {
+// function getUser(callback) {
+//    const userId = callback(Date.now());
+//    return {
+//       name: `john`,
+//       id: userId,
+//    };
+// }
+
+// const user = getUser(getUserId);
+// // console.log(user);
+
+// const users = [
+//    {
+//       name: `john`,
+//       isActive: true,
+//    },
+//    {
+//       name: `mike`,
+//       isActive: true,
+//    },
+//    {
+//       name: `michelle`,
+//       isActive: false,
+//    },
+// ];
+
+// for (let i = 0; i < users.length; i++) {
+//    //this does what the arrow function below does
+//    const user = users[i];
+//    console.log(user.name);
+// }
+
+// users.forEach(function (user) {
+//    //anonymous function
+//    //    console.log(user.name);
+// });
+
+// users.forEach((user) => {
+//    //arrow function, same as anonymous function except uses arrow syntax
+//    //    console.log(user);
+// });
+
+const users = [
+   {
       name: `john`,
-      id: userId,
-   };
-}
+      isActive: true,
+   },
+   {
+      name: `mike`,
+      isActive: true,
+   },
+   {
+      name: `michelle`,
+      isActive: false,
+   },
+];
 
-const user = getUser(getUserId);
-console.log(user);
+let activeUsers = [];
+
+users.forEach((user) => {
+   if (user.isActive) {
+      activeUsers = activeUsers.concat(user);
+   }
+});
+console.log(activeUsers);
+
+// users.forEach(() => {
+//    //always set up like this
+//    //whatever you want to do
+//    //this is technically a side effect
+// });
--- test.js
+++ test.js
@@ -1,20 +1,9 @@
-const testString = toReverse("Hello");
-console.log(`Here is the reversed test string: `, testString); // returns o,l,l,e,H
-function toReverse(str) {
-   if (str === "") {
-      throw Error("String cannot be empty.");
-   }
-   try {
-      const chars = str.split("");
-      console.log(`Split chars: `, chars); // returns ["H", "e", "l", "l", "o"]
-      const copyOfChars = [...chars];
-      console.log(`spread chars: `, copyOfChars); // returns ["H", "e", "l", "l", "o"]
-      const reversedChars = copyOfChars.reverse();
-      console.log(`reversed chars: `, reversedChars); // returns ["o", "l", "l", "e", "H"]
-      const reversedStr = reversedChars.join("");
-      console.log(`join characters: `, reversedChars); // returns ["o", "l", "l", "e", "H"]
-      return reversedStr;
-   } catch {
-      return "Error: there is a problem with toReverse().";
-   }
-}
+unacceptablePasswordsForwardAndReversed.forEach((password) => {
+   let passwordChars = unacceptablePasswordStrings[i].split(``);
+   const copyOfPasswordChars = [...passwordChars];
+   const reversePasswordChars = copyOfPasswordChars.reverse();
+   const password = reversePasswordChars.join(``);
+   unacceptablePasswordsForwardAndReversed = unacceptablePasswordsForwardAndReversed.concat(
+      password
+   );
+});
