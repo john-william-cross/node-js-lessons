ea867f5aaa4278043cd99170f76876886f9756c7
John Cross <john.william.cross@gmail.com>
2020-10-05 08:15:13 -0700

    begin filter.js notes

---
+++ filter.js
@@ -0,0 +1,5 @@
+const users = [
+   { name: "John", isActive: true, createdAt: 1601910841120 },
+   { name: "Mike", isActive: true, createdAt: 1601910841130 },
+   { name: "Michelle", isActive: false, createdAt: 1601910841150 },
+];
--- map.js
+++ map.js
@@ -18,16 +18,24 @@ const users = [
 
 // const newUsers = users.map(() => {}) skeleton of map function
 // map returns a new array doing whatever changes you ask it to
-const newUsers = users.map((user) => {
-   //for every user, in this list of users, return a user
-   return user.name; //returns just a list of objects as a list of strings
-   const newUser = { ...user }; // if you do something inside the code block it will update the old array
-   // we have to make a copy!!!
-   newUser.createdAt = Date.now(); //adds a new property to user called createdAt
-   delete newUser.isActive; // removes a property
-   newUser.name = user.name.toLowerCase(); // updates a property
+// const newUsers = users.map((user) => {
+//    //for every user, in this list of users, return a user
+//    return user.name; //returns just a list of objects as a list of strings
+//    const newUser = { ...user }; // if you do something inside the code block it will update the old array
+//    // we have to make a copy!!!
+//    newUser.createdAt = Date.now(); //adds a new property to user called createdAt
+//    delete newUser.isActive; // removes a property
+//    newUser.name = user.name.toLowerCase(); // updates a property
+//    return newUser;
+// });
+
+// console.log(`new users: `, newUsers);
+// console.log(`users: `, users);
+
+const formattedUsers = users.map((user) => {
+   const newUser = { ...user };
+   newUser.createdAt = Date.now();
    return newUser;
 });
 
-console.log(`new users: `, newUsers);
-console.log(`users: `, users);
+console.log(formattedUsers);
