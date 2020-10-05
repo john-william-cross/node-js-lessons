417f2af9fb0f714c19852d43f95053a3ca5c97c9
John Cross <john.william.cross@gmail.com>
2020-10-05 09:36:12 -0700

    finish this challenge

--- filter.js
+++ filter.js
@@ -1,5 +1,30 @@
 const users = [
-   { name: "John", isActive: true, createdAt: 1601910841120 },
+   { name: "John", isActive: true, createdAt: 1601910841140 },
    { name: "Mike", isActive: true, createdAt: 1601910841130 },
    { name: "Michelle", isActive: false, createdAt: 1601910841150 },
 ];
+
+//get just active users
+
+//filter returns a new array just like map.
+
+//filter EXPECTS something to come across as true or false
+//In the code below, if something is true it will add it to
+//the new activeUsers list
+
+//Filter function is ALWAYS waiting for true or false.
+
+//each array method we learn has different things happening
+
+const activeUsers = users.filter((user) => {
+   //    return user.name === `Michelle`; //evaluates to false on two
+   //of the objects and true on one of them
+
+   //if you wanted to return most recent users:
+   // filter for created after or equal to john created at
+   return user.createdAt >= 1601910841140;
+
+   return user.isActive; //returns john and mike since isActive === true
+});
+
+console.log(activeUsers);
