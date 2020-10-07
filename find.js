4babf60aedde8765847804d6135139468c622198
John Cross <john.william.cross@gmail.com>
2020-10-07 08:09:28 -0700

    begin find.js notes

---
+++ find.js
@@ -0,0 +1,12 @@
+const authors = [
+   { name: `Philip Roth`, books: [{ title: `American Pastoral` }] },
+   {
+      name: `Franz Kafka`,
+      books: [{ title: `The Castle` }, { title: `The Hunger Artist` }],
+   },
+   {
+      name: `Margaret Atwood`,
+      books: [{ title: `Oryx and Crake` }, { title: `The Year of the Flood` }],
+   },
+   { name: `Philip Roth`, books: [{ title: `Sabbath's Theater` }] },
+];
