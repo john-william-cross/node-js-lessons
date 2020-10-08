886ae1b376261edf12b2f22f203e28604b49c735
John Cross <john.william.cross@gmail.com>
2020-10-07 20:45:16 -0700

    post regex solution

--- regex-methods.js
+++ regex-methods.js
@@ -1,1 +1,6 @@
-const regex = /REGEX/;
+const regex = /REGEX/gm; // gm= global multiline
+
+//way to search within a string
+
+//regex solution comment
+// ^[0-9a-zA-Z_].*@[a-zA-Z0-9]{1}\w+-*x?.[a-zA-Z]{2,}$
