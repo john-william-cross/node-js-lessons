23bfef95d92a5934b558384691390c9cfbe82e62
John Cross <john.william.cross@gmail.com>
2020-11-10 11:18:46 -0800

    begin fizz buzz

---
+++ fizzbuzz.js
@@ -0,0 +1,15 @@
+let number;
+
+function fizzBuzz(num) {
+   for (let i = 01; i < num + 1; i++) {
+      console.log(i);
+   }
+}
+
+fizzBuzz(15);
+
+// Write a program that generates an array of integers from 1 to 100 (inclusive). But:
+
+// for multiples of 3, add "Fizz" to the array instead of the number
+// for multiples of 5, add "Buzz" to the array instead of the number
+// for multiples of 3 and 5, add "FizzBuzz" to the array instead of the number
