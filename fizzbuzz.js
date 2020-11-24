3d06b8c744bcd280d3fb91b05cc7ed5cb19306a5
John Cross <john.william.cross@gmail.com>
2020-11-24 08:50:31 -0800

    console.log(nums)

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,6 +4,7 @@ for (let i = 0; i < 100; i++) {
    // starting at index i, set the value of index i to i + 1
    nums[i] = i + 1;
 }
+console.log(nums);
 
 const fizzBuzzArray = nums.map((num) => {
    if (num % 3 === 0 && num % 5 === 0) {
